8503074c6b294418b28c860e506bffea778f951c
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2023-10-14 20:54:13 +0700

    feat: CRUD user

---
+++ js/login-signup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var icon = document.querySelector('.fa-circle-user');
+    var loginForm = document.querySelector('.loginForm-hidden');
+
+    // icon.addEventListener('click', function() {
+    //     loginForm.classList.toggle('hidden');
+    // });
+});
