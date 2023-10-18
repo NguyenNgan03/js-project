42dfe0d1f045ec27de1293a73223636c36d407f3
KhanhNhi <nhi.tuong25@student.passerellesnumeriques.org>
2023-10-18 18:31:26 +0700

    productAmdin

---
+++ js/productAdmin.js
@@ -0,0 +1,25 @@
+const product = {
+    name: '',
+    quantity:0,
+    price:0,
+    discount:0,
+    location:''
+}
+const allproduct=[];
+
+// fix cứng data
+let products = [
+    { id: 1, nameProduct:"Bảo tàng Chăm", quantity:"20", price: "$123.85", discount:"30%", location:" Hải Châu- Đà Nẵng" },
+    { id: 2, nameProduct:"Bảo tàng Lịch sử VN", quantity:"21", price: "$154,83", discount:"10%", location:" Hoàn Kiếm- Hà Nội" }
+];
+
+// build function
+const addProduct =()=>{
+
+}
+const editProduct =() =>{
+
+}
+const deleteProduct =() =>{
+
+}
