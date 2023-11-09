4791d1d72a3e2d0378dbb3f748a14a8c13a083d9
Hai con cò <142873464+NguyenNgan03@users.noreply.github.com>
2023-11-09 23:15:25 +0700

    Aboutus (#49)
    * update detail and booking
    
    * update about us home

--- js/home.js
+++ js/home.js
@@ -12,16 +12,18 @@ function displayProduct(){
       }
       product+=`
       <div class="col-md-3">
+      <a href="./index.html?id=${item.id}" class="change">
       <div class="card" style="width: 18rem;">
-        <img src="${item.img1}" class="card-img-top" alt="${item.img1}">
+        <img src="${item.img1}" class="card-img-top imagge1" alt="${item.name}">
         <div class="card-body">
-          <h5 class="card-title">${item.name}</h5>
-          <p class="card-text">${item.firstDescribe}</p>
+          <h5 class="card-title title">${item.name}</h5>
+          <p class="card-text description">${item.firstDescribe}</p>
           <p class="card-text"> ${item.location}</p>
           <p class="card-text"> ${item.price}</p>
-          <a href="./index.html?id=${item.id}" class="btn btn-primary">Booking</a>
+          <a href="./index.html?id=${item.id}" class="btn btn-primary booking">Booking</a>
         </div>
       </div>
+      </a>
       </div>
       `;
       if (productCount % 4 === 3 || productCount === data.length - 1) {
@@ -37,3 +39,58 @@ function displayProduct(){
   })
 }
 displayProduct();
+
+// Đợi cho trang tải xong
+document.addEventListener("DOMContentLoaded", function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get('id');
+
+  // Tạo hàm tìm kiếm sản phẩm
+  function searchProducts(keyword) {
+      // Lọc sản phẩm theo từ khóa tìm kiếm
+      const filteredProducts = data.filter(product => {
+          // Ở đây, bạn có thể chọn các trường thông tin để tìm kiếm, ví dụ: name, location, firstDescribe, ...
+          return product.name.toLowerCase().includes(keyword.toLowerCase()) ||
+                 product.location.toLowerCase().includes(keyword.toLowerCase()) ||
+                 product.firstDescribe.toLowerCase().includes(keyword.toLowerCase());
+      });
+
+      // Hiển thị kết quả tìm kiếm
+      displaySearchResults(filteredProducts);
+  }
+
+  // Hiển thị kết quả tìm kiếm
+  function displaySearchResults(results) {
+      let searchResultContainer = ``;
+
+      if (results.length > 0) {
+          results.forEach(product => {
+              searchResultContainer += `
+              <!-- Tạo giao diện hiển thị sản phẩm tìm kiếm -->
+              <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                  <img src="${item.img1}" class="card-img-top" alt="${item.img1}">
+                  <div class="card-body">
+                    <h5 class="card-title">${item.name}</h5>
+                    <p class="card-text">${item.firstDescribe}</p>
+                    <p class="card-text"> ${item.location}</p>
+                    <p class="card-text"> ${item.price}</p>
+                    <a href="./index.html?id=${item.id}" class="btn btn-primary">Booking</a>
+                  </div>
+                </div>
+              </div>
+              `;
+          });
+      } else {
+          searchResultContainer = "Không tìm thấy kết quả.";
+      }
+
+      document.getElementById('search-results').innerHTML = searchResultContainer;
+  }
+
+  // Bắt sự kiện click nút tìm kiếm
+  document.getElementById('searchButton').addEventListener('click', function() {
+      const keyword = document.getElementById('searchInput').value;
+      searchProducts(keyword);
+  });
+});
--- js/index.js
+++ js/index.js
@@ -9,7 +9,67 @@ function displayProductById(productId){
         const product = data.find(item=> item.id === parseInt(productId));
         if(product){
            productContainer += `
-
+           <div id = "contain">
+            <div class = "card">
+            <!-- card left -->
+            <div class = "" id="change2">
+                <div class = "img-display">
+                    <h1 id="title1">${product.name}</h1>
+                    <div class = "img-showcase second_img_box">
+                        <img src = "${product.img1}" alt = "shoe image" class="second_img">
+                        <img src = "${product.img2}" alt = "shoe image" class="second_img">
+                        <img src = "${product.img3}" alt = "shoe image" class="second_img">
+                        <img src = "${product.img4}" alt = "shoe image" class="second_img">
+                    </div>
+                </div>
+                <div class = "img-select" id="change3">
+                    <div class = "img-item">
+                        <a href = "#" data-id = "1">
+                        <img src = "${product.img1}" alt = "shoe image">
+                        </a>
+                    </div>
+                    <div class = "img-item">
+                        <a href = "#" data-id = "2">
+                        <img src = "${product.img2}" alt = "shoe image">
+                        </a>
+                    </div>
+                    <div class = "img-item">
+                        <a href = "#" data-id = "3">
+                        <img src = "${product.img3}" alt = "shoe image">
+                        </a>
+                    </div>
+                    <div class = "img-item">
+                        <a href = "#" data-id = "4">
+                        <img src = "${product.img4}" alt = "shoe image">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- card right -->
+            <div class = "product-content" id="change4">
+                <a href = "#" id="product_title" class = "product-title">${product.firstDescribe}</a>
+                <div class = "product-price">
+                    <p class="new-price"> Price: <span>${product.price}</span></p>
+                    <p><i style = "color:#FFC300" class="fa fa-clock"></i>Tình trạng: <span>Đang mở cửa</span></p>
+                        <div class = "product-rating">
+                            <p><i style = "color:#FFC300" class="fa fa fa-check-circle"></i> Đánh giá: 
+                                <i style="color: black;" > ${product.asess}</i>
+                            </p>
+                        </div>
+                    <p><i style = "color:#FFC300" class="fa fa-map-marker-alt"></i> Địa chỉ: <span>123 Đường ABC, Thành phố XYZ</span></p>
+                    <p><i style = "color:#FFC300" class="fa fa-phone"></i> Hotline: <span>${product.hotline}</span></p>
+                </div>
+                <div>
+                    <p class="title">Description</p>
+                    <P> ${product.seconDescribe} </P>
+                </div>
+                <div class="purchase-info">
+                    <a href="../html/bookTicket.html">
+                        <button type = "button" class = "btn"> <a href="./bookTicket.html?id=${product.id}" class = "booking"> Booking </a> </button>
+                    </a>
+                </div>
+            </div>
+            </div>
            <div class = "card-wrapper">
             <div class = "card">
             <!-- card left -->
@@ -70,7 +130,6 @@ function displayProductById(productId){
                     </a>
                 </div>
             </div>
-
         </div>
     </div>
            `;
